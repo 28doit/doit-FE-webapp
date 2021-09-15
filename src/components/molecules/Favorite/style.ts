36e4c90d010ee0c3f837ef96b722af9e9ead67db
poeynus <poeynus@gmail.com>
2021-09-15 14:09:41 +0900

    Add molecules - Favorite => style

---
+++ src/components/molecules/Favorite/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Btn } from '../../index';
+
+export const MyFContainer = styled.div``;
+
+export const MyFTitle = styled.h2``;
+
+export const MyFImgBtnYes = styled(Btn)``;
+
+export const MyFImgBtnNo = styled(Btn)``;
+
+export const MyFAuthorBtnYes = styled(Btn)``;
+
+export const MyFAuthorBtnNo = styled(Btn)``;
+
+export const MyFImgBox = styled.div``;
+
+export const MyFAuthorBox = styled.div``;
