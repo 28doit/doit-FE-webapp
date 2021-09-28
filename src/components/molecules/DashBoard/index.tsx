87e624cefa016fd09c0f1d5f6dd6488bced33542
poeynus <poeynus@gmail.com>
2021-09-28 23:10:06 +0900

    Add DashBoard - css

--- src/components/molecules/DashBoard/index.tsx
+++ src/components/molecules/DashBoard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as S from './style';
+import { Card } from '../Card/index';
 
 export const DashBoardItem = (): React.ReactElement => {
   return (
@@ -9,10 +10,22 @@ export const DashBoardItem = (): React.ReactElement => {
         <S.TopNikName>poeynus</S.TopNikName>
       </S.DashBoardTop>
       <S.DashBoardMid>
-        <S.MidRate></S.MidRate>
-        <S.MidSale></S.MidSale>
-        <S.MidIng></S.MidIng>
-        <S.MidFan></S.MidFan>
+        <S.MidBox>
+          <S.MidP>누적 판매 수</S.MidP>
+          <S.MidP>1</S.MidP>
+        </S.MidBox>
+        <S.MidBox>
+          <S.MidP>누적 매출</S.MidP>
+          <S.MidP>1</S.MidP>
+        </S.MidBox>
+        <S.MidBox>
+          <S.MidP>판매중 콘텐츠</S.MidP>
+          <S.MidP>1</S.MidP>
+        </S.MidBox>
+        <S.MidBox>
+          <S.MidP>팬 수</S.MidP>
+          <S.MidP>1</S.MidP>
+        </S.MidBox>
       </S.DashBoardMid>
       <S.DashBoardBot>
         <S.BotUl></S.BotUl>
--- src/components/molecules/DashBoard/style.ts
+++ src/components/molecules/DashBoard/style.ts
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 
 export const DashBoardContainer = styled.div`
   width: 100%;
+  height: 120%;
   display: flex;
   align-items: center;
   flex-direction: column;
 `;
 
 export const DashBoardTop = styled.div`
+  width: 100%;
   display: flex;
+  background-color: #6f6768;
 `;
 
 export const TopProfileImg = styled.img`
@@ -21,7 +24,27 @@ export const TopNikName = styled.div`
   font-weight: 700;
 `;
 
-export const DashBoardMid = styled.div``;
+export const DashBoardMid = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const MidBox = styled.div`
+  width: 200px;
+  background-color: #3b8e83;
+  height: 160px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MidP = styled.p`
+  color: white;
+  padding-top: 20px;
+`;
 
 export const MidRate = styled.div``;
 
@@ -31,7 +54,10 @@ export const MidIng = styled.div``;
 
 export const MidFan = styled.div``;
 
-export const DashBoardBot = styled.div``;
+export const DashBoardBot = styled.div`
+  width: 100%;
+  background-color: #f3f0f0;
+`;
 
 export const BotUl = styled.ul``;
 
